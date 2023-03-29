67b91fe6ca20a004e5a9c0fa0f878a04c395cdf4
gokhangunduz <mail@gokhangunduz.com.tr>
2023-03-29 18:32:08 +0300

    refactor(ForgotPassword): :tada: Update forgot password layout

--- src/keycloak-theme/login/KcApp.tsx
+++ src/keycloak-theme/login/KcApp.tsx
@@ -7,7 +7,7 @@ import { useI18n } from "./i18n";
 const Template = lazy(() => import("./Template"));
 const DefaultTemplate = lazy(() => import("keycloakify/login/Template"));
 
-// You can uncomment this to see the values passed by the main app before redirecting.  
+// You can uncomment this to see the values passed by the main app before redirecting.
 //import { foo, bar } from "./valuesTransferredOverUrl";
 //console.log(`Values passed by the main app in the URL parameter:`, { foo, bar });
 
@@ -15,52 +15,108 @@ const Login = lazy(() => import("./pages/Login"));
 // If you can, favor register-user-profile.ftl over register.ftl, see: https://docs.keycloakify.dev/realtime-input-validation
 const Register = lazy(() => import("./pages/Register"));
 const RegisterUserProfile = lazy(() => import("./pages/RegisterUserProfile"));
+const LoginResetPassword = lazy(() => import("./pages/LoginResetPassword"));
 const Terms = lazy(() => import("./pages/Terms"));
 const MyExtraPage1 = lazy(() => import("./pages/MyExtraPage1"));
 const MyExtraPage2 = lazy(() => import("./pages/MyExtraPage2"));
 const Info = lazy(() => import("keycloakify/login/pages/Info"));
 
-// This is like adding classes to theme.properties 
+// This is like adding classes to theme.properties
 // https://github.com/keycloak/keycloak/blob/11.0.3/themes/src/main/resources/theme/keycloak/login/theme.properties
 const classes: PageProps<any, any>["classes"] = {
-    // NOTE: The classes are defined in ./KcApp.css
-    "kcHtmlClass": "my-root-class",
-    "kcHeaderWrapperClass": "my-color my-font"
+  // NOTE: The classes are defined in ./KcApp.css
+  kcHtmlClass: "my-root-class",
+  kcHeaderWrapperClass: "my-color my-font",
 };
 
-export default function App(props: { kcContext: KcContext; }) {
+export default function App(props: { kcContext: KcContext }) {
+  const { kcContext } = props;
 
-    const { kcContext } = props;
+  const i18n = useI18n({ kcContext });
 
-    const i18n = useI18n({ kcContext });
+  if (i18n === null) {
+    //NOTE: Locales not yet downloaded, we could as well display a loading progress but it's usually a matter of milliseconds.
+    return null;
+  }
 
-    if (i18n === null) {
-        //NOTE: Locales not yet downloaded, we could as well display a loading progress but it's usually a matter of milliseconds.
-        return null;
-    }
+  /*
+   * Examples assuming i18n.currentLanguageTag === "en":
+   * i18n.msg("access-denied") === <span>Access denied</span>
+   * i18n.msg("foo") === <span>foo in English</span>
+   */
 
-    /* 
-    * Examples assuming i18n.currentLanguageTag === "en":
-    * i18n.msg("access-denied") === <span>Access denied</span>
-    * i18n.msg("foo") === <span>foo in English</span>
-    */
-
-    return (
-        <Suspense>
-            {(() => {
-                switch (kcContext.pageId) {
-                    case "login.ftl": return <Login {...{ kcContext, i18n, Template, classes }} doUseDefaultCss={true} />;
-                    case "register.ftl": return <Register {...{ kcContext, i18n, Template, classes }} doUseDefaultCss={true} />;
-                    case "register-user-profile.ftl": return <RegisterUserProfile {...{ kcContext, i18n, Template, classes }} doUseDefaultCss={true} />
-                    case "terms.ftl": return <Terms {...{ kcContext, i18n, Template, classes }} doUseDefaultCss={true} />;
-                    case "my-extra-page-1.ftl": return <MyExtraPage1 {...{ kcContext, i18n, Template, classes }} doUseDefaultCss={true} />;
-                    case "my-extra-page-2.ftl": return <MyExtraPage2 {...{ kcContext, i18n, Template, classes }} doUseDefaultCss={true} />;
-                    // We choose to use the default Template for the Info page and to download the theme resources.
-                    case "info.ftl": return <Info {...{ kcContext, i18n, classes }} Template={DefaultTemplate} doUseDefaultCss={true} />;
-                    default: return <Fallback {...{ kcContext, i18n, classes }} Template={DefaultTemplate} doUseDefaultCss={true} />;
-                }
-            })()}
-        </Suspense>
-    );
-
+  return (
+    <Suspense>
+      {(() => {
+        switch (kcContext.pageId) {
+          case "login.ftl":
+            return (
+              <Login
+                {...{ kcContext, i18n, Template, classes }}
+                doUseDefaultCss={true}
+              />
+            );
+          case "register.ftl":
+            return (
+              <Register
+                {...{ kcContext, i18n, Template, classes }}
+                doUseDefaultCss={true}
+              />
+            );
+          case "login-reset-password.ftl":
+            return (
+              <LoginResetPassword
+                {...{ kcContext, i18n, Template, classes }}
+                doUseDefaultCss={true}
+              />
+            );
+          case "register-user-profile.ftl":
+            return (
+              <RegisterUserProfile
+                {...{ kcContext, i18n, Template, classes }}
+                doUseDefaultCss={true}
+              />
+            );
+          case "terms.ftl":
+            return (
+              <Terms
+                {...{ kcContext, i18n, Template, classes }}
+                doUseDefaultCss={true}
+              />
+            );
+          case "my-extra-page-1.ftl":
+            return (
+              <MyExtraPage1
+                {...{ kcContext, i18n, Template, classes }}
+                doUseDefaultCss={true}
+              />
+            );
+          case "my-extra-page-2.ftl":
+            return (
+              <MyExtraPage2
+                {...{ kcContext, i18n, Template, classes }}
+                doUseDefaultCss={true}
+              />
+            );
+          // We choose to use the default Template for the Info page and to download the theme resources.
+          case "info.ftl":
+            return (
+              <Info
+                {...{ kcContext, i18n, classes }}
+                Template={DefaultTemplate}
+                doUseDefaultCss={true}
+              />
+            );
+          default:
+            return (
+              <Fallback
+                {...{ kcContext, i18n, classes }}
+                Template={DefaultTemplate}
+                doUseDefaultCss={true}
+              />
+            );
+        }
+      })()}
+    </Suspense>
+  );
 }
